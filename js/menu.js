d64fb687b6f501c3d1c85cdc6187d81e94c059e6
chaile gustavo <gustavochaile7@gmail.com>
2019-09-12 08:11:56 -0300

    se relizo cambios en hero de menu

--- js/menu.js
+++ js/menu.js
@@ -2,8 +2,29 @@ $(document).ready(menu);
 
 function menu(){
 	$('.menu-responsive').on('click', cambio);
+
+	$(window).on('scroll',function(){
+
+		var scrollTop = $('html').scrollTop();
+		
+
+		if( scrollTop > 400 ){
+
+			$('header').addClass('menu-fixed');
+
+		} else {
+
+			$('header').removeClass('menu-fixed');
+
+		}
+
+
+
+	});
+
 }
 
 function cambio(){
 	$('nav').slideToggle();
 }
+
