4a57829d5d9b32efecdc82edf3189953940e43a5
Nicolas Cedron <josenicolascedron@gmail.com>
2019-09-20 23:32:43 -0300

    Fix Super Header

--- js/menu.js
+++ js/menu.js
@@ -6,22 +6,15 @@ function menu(){
 	$(window).on('scroll',function(){
 
 		var scrollTop = $('html').scrollTop();
-		console.log(scrollTop)
 
 		if( scrollTop >= 72 && !$('.menu-wrapper').hasClass('menu-fixed')){
 			$('.menu-wrapper').addClass('menu-fixed');
 
-			setTimeout(function(){
-				$('.menu-wrapper').addClass('effect');
-			}, 100);
 
 		}else if (scrollTop == 0 && $('.menu-wrapper').hasClass('menu-fixed')){
 
-			setTimeout(function(){
-				$('.menu-wrapper').removeClass('effect');
-				$('.menu-wrapper').removeClass('menu-fixed');
-			}, 100);
-			
+			$('.menu-wrapper').removeClass('menu-fixed');
+	
 		}
 
 
